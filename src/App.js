95cf021de57afac0ed84714227f2e14cbe18f080
darkocobbygh <darkogaucho@gmail.com>
2023-11-07 13:16:30 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+      <p>
+      I am your Emmanuel Darko Cobblah, this is my first React application
+      </p>
+    </div>
+  );
+}
+
+export default App;
